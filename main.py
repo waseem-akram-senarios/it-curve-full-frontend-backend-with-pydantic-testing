9aef570615310f83c2b8922722d63fda231d1d2a
shakeelahmed05 <shakeel.ahmed@ncspvt.com>
2025-07-25 21:38:34 +0500

    openai temperature value updated to 0.1

--- main.py
+++ main.py
@@ -422,7 +422,7 @@ async def entrypoint(ctx: agents.JobContext):
     session = AgentSession(
         stt = deepgram.STT(model="nova-3",language="en-US",keyterms=["snouffer"]),
         allow_interruptions=allow_interruption_status,
-        llm=openai.LLM(model="gpt-4o", temperature=0.3),
+        llm=openai.LLM(model="gpt-4o", temperature=0.1),
         tts=deepgram.TTS(model="aura-asteria-en"),
         vad=silero.VAD.load(min_silence_duration=1.0),
         # min_interruption_duration=1.0,
