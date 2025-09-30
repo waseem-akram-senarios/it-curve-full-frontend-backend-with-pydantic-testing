20aa3cd39ed61b2ed5667e95d867c8170d5dc740
sehar-aidevlab <sehar@aidevlab.com>
2025-09-30 21:50:40 +0500

    fixed hardcoded numbers

--- main.py
+++ main.py
@@ -521,10 +521,9 @@ Remember: You are ONLY here to assist with transportation services for the agenc
             # For Asterisk
             logger.info(participant.attributes)
             # metadata = participant.metadata
-            # caller = participant.attributes['sip.phoneNumber']
-            # recipient = participant.attributes['sip.trunkPhoneNumber']
-            caller = "+13854156545"
-            recipient = "+19383481263"
+            caller = participant.attributes['sip.phoneNumber']
+            recipient = participant.attributes['sip.trunkPhoneNumber']
+            
             # Try to get affiliate from cache
             cached_affiliate = cache_manager.get_affiliate_from_cache(recipient)
             print("cached_affiliate ",cached_affiliate)
